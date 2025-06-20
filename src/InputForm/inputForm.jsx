0c8b448ebddd964cb3b4378f484a6aa7731e26ff
Lewis Wootton <111094589+L-E-W-1-5@users.noreply.github.com>
2025-06-20 14:38:25 +0100

    Update inputForm.jsx
    Answer.payload will have a value once sent line 97

--- src/InputForm/inputForm.jsx
+++ src/InputForm/inputForm.jsx
@@ -94,7 +94,7 @@ const InputForm = () => {
 
             </form>
 
-            {emailVisibility && <EmailForm emailVisibility={setEmailVisibility} answer={answer.payload}></EmailForm>}
+            {emailVisibility && <EmailForm emailVisibility={setEmailVisibility} answer={answer.payload ? answer.payload : ""}></EmailForm>}
 
         </div>
 
