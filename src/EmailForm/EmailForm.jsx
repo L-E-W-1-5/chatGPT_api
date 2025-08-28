ead8ea71b383361fdb3b2726381897e072813a82
Lewis Wootton <111094589+L-E-W-1-5@users.noreply.github.com>
2025-08-28 23:39:11 +0100

    Update EmailForm.jsx
    Line 109 to 111

--- src/EmailForm/EmailForm.jsx
+++ src/EmailForm/EmailForm.jsx
@@ -106,9 +106,9 @@ const EmailForm = ({emailVisibility, answer}) => {
 
             .then(res => res.json())
 
-            .then(res => res.success === false ? alert(res) : alert("Email Sent!"))
+            .then(res => res.success === false ? alert("res.payload") : alert("Email Sent!"))
 
-            .then(res => console.log(res))
+            .then(res => alert(res))
 
             .catch(err => console.log(err))
 
